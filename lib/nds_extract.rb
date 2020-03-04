7f36c39f5671f5467b2f59bba28d5b910796a267
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-04 16:38:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,7 +37,7 @@ def directors_totals(nds)
 
   # build result hash by reducing directors array
   # using the :name String value as the hash key.
-  pp nds.reduce({}) { |r, d| r[d[:name]] = d[:movies].map{|m| m[:worldwide_gross]}}
+  pp nds.reduce({}) { |r, d| r[d[:name]] = 0 }
 
 
 
